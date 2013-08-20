778ca57cdd567c409e5858159161d4dd9a335425
objectundefined <gabriel.lipson@gmail.com>
2013-08-20 11:23:03 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var EventEmitter = require('events').EventEmitter;
+
+function CompletionEmitter ( opts ) {
+  
+  this.numSteps = opts && opts.steps || 0 ;
+  this.uptickAt = opts && opts.uptickAt || 1 ;
+  this.numCompleted = 0 ;
+  this.lastLoggedCompletion = -1 ;
+  
+}
+
+CompletionEmitter.prototype.__proto__ = EventEmitter.prototype;
+
+CompletionEmitter.prototype.setSteps = function(steps){
+  
+  this.numSteps = steps ;
+  
+};
+
+CompletionEmitter.prototype.increment = function(){
+  
+  this.numCompleted += 1 ;
+  
+  totalCompletion = Math.floor((this.numCompleted / this.numSteps )* 100) ;
+  
+  totalCompletion -= (totalCompletion%this.uptickAt) ;
+  
+  if (totalCompletion>this.lastLoggedCompletion && totalCompletion%this.uptickAt===0 ) {
+    
+    this.lastLoggedCompletion = totalCompletion ;
+    
+    this.emit('uptick',this.lastLoggedCompletion);
+    
+  }
+  
+};
+
+
+module.exports = CompletionEmitter ;
